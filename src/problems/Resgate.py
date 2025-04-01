09bbef6874bdfc9f921d18ccd9b60de3d8c82e47
NunoRodriguesS4Y <nuno.rodrigues@systems4you.pt>
2025-04-01 19:39:30 +0100

    Implement Rescue (untested)

--- src/problems/Resgate.py
+++ src/problems/Resgate.py
@@ -1,13 +1,14 @@
 from ..searchAlgorithms.helpers.Problem import Problem
 from searchAlgorithms.helpers.Node import Node
+from dataclasses import dataclass
 
 
+@dataclass(frozen=True)
 class RescueState:
-    def __init__(self):
-        self.peopleRescued = 0
-        self.coordinates = (0, 0)
-        self.teamLeft = False
-        self.timeLeft = 0
+    peopleRescued: int
+    coordinates: tuple
+    teamLeft: bool
+    timeLeft: int
 
 
 class Rescue(Problem):
@@ -28,45 +29,148 @@ class Rescue(Problem):
         self._goalNode: Node | None = None
 
     def _moveUp(self, node: Node):
-        cost = 0
-        teamLeft = False
-
         x, y = node.state.coordinates
-        y -= 1
+        new_y = y - 1
 
-        if y < 0:
+        if new_y < 0:
             if x == self._parkSize // 2:
-                cost = 1
-                teamLeft = True
-        else:
+                timeLeft = node.state.timeLeft - 1
+
+                if timeLeft < 0:
+                    return None
+
+                newState = RescueState(
+                    node.state.peopleRescued, (x, new_y), True, timeLeft
+                )
+                newNode = Node(newState, node)
+                newNode.cost = node.cost + 1
+                return newNode
+            else:
+                return None
+
+        if self._park[new_y][x] == 10:
             return None
 
-        if self._park[y][x] == 10:
+        cost = self._park[new_y][x]
+        timeLeft = node.state.timeLeft - cost
+
+        if timeLeft < 0:
             return None
 
-        cost = self._park[y][x]
+        peopleRescued = node.state.peopleRescued + (cost < 0)
 
-        newState = RescueState()
-        newState.timeLeft -= cost
-        if newState.timeLeft >= 0:
-            return None
-
-        newState.peopleRescued += cost < 0
-        newState.coordinates = (x, y)
-        newState.teamLeft = teamLeft
-
+        newState = RescueState(peopleRescued, (x, new_y), False, timeLeft)
         newNode = Node(newState, node)
         newNode.cost = node.cost + cost
         return newNode
 
     def _moveDown(self, node: Node):
-        pass
+        x, y = node.state.coordinates
+        new_y = y + 1
+
+        if new_y == self._parkSize:
+            if x == self._parkSize // 2:
+                timeLeft = node.state.timeLeft - 1
+
+                if timeLeft < 0:
+                    return None
+
+                newState = RescueState(
+                    node.state.peopleRescued, (x, new_y), True, timeLeft
+                )
+                newNode = Node(newState, node)
+                newNode.cost = node.cost + 1
+                return newNode
+            else:
+                return None
+
+        if self._park[new_y][x] == 10:
+            return None
+
+        cost = self._park[new_y][x]
+        timeLeft = node.state.timeLeft - cost
+
+        if timeLeft < 0:
+            return None
+
+        peopleRescued = node.state.peopleRescued + (cost < 0)
+
+        newState = RescueState(peopleRescued, (x, new_y), False, timeLeft)
+        newNode = Node(newState, node)
+        newNode.cost = node.cost + cost
+        return newNode
 
     def _moveLeft(self, node: Node):
-        pass
+        x, y = node.state.coordinates
+        new_x = x - 1
+
+        if new_x < 0:
+            if y == self._parkSize // 2:
+                timeLeft = node.state.timeLeft - 1
+
+                if timeLeft < 0:
+                    return None
+
+                newState = RescueState(
+                    node.state.peopleRescued, (new_x, y), True, timeLeft
+                )
+                newNode = Node(newState, node)
+                newNode.cost = node.cost + 1
+                return newNode
+            else:
+                return None
+
+        if self._park[y][new_x] == 10:
+            return None
+
+        cost = self._park[y][new_x]
+        timeLeft = node.state.timeLeft - cost
+
+        if timeLeft < 0:
+            return None
+
+        peopleRescued = node.state.peopleRescued + (cost < 0)
+
+        newState = RescueState(peopleRescued, (new_x, y), False, timeLeft)
+        newNode = Node(newState, node)
+        newNode.cost = node.cost + cost
+        return newNode
 
     def _moveRight(self, node: Node):
-        pass
+        x, y = node.state.coordinates
+        new_x = x + 1
+
+        if new_x == self._parkSize:
+            if y == self._parkSize // 2:
+                timeLeft = node.state.timeLeft - 1
+
+                if timeLeft < 0:
+                    return None
+
+                newState = RescueState(
+                    node.state.peopleRescued, (new_x, y), True, timeLeft
+                )
+                newNode = Node(newState, node)
+                newNode.cost = node.cost + 1
+                return newNode
+            else:
+                return None
+
+        if self._park[y][new_x] == 10:
+            return None
+
+        cost = self._park[y][new_x]
+        timeLeft = node.state.timeLeft - cost
+
+        if timeLeft < 0:
+            return None
+
+        peopleRescued = node.state.peopleRescued + (cost < 0)
+
+        newState = RescueState(peopleRescued, (new_x, y), False, timeLeft)
+        newNode = Node(newState, node)
+        newNode.cost = node.cost + cost
+        return newNode
 
     @property
     def goalState(self):
@@ -74,13 +178,43 @@ class Rescue(Problem):
 
     @property
     def goalNode(self) -> Node | None:
-        raise NotImplementedError("goalNode must be implemented in a subclass")
+        return self._goalNode
 
     def isGoal(self, node: Node) -> bool:
-        raise NotImplementedError("isGoal must be implemented in a subclass")
+        s = node.state
+
+        if s.peopleRescued != self._objective:
+            return False
+
+        if not s.teamLeft:
+            return False
+
+        if s.timeLeft < 0:
+            return False
+
+        self._goalNode = node
+        return True
 
     def expand(self, node: Node) -> list[Node]:
-        raise NotImplementedError("expand must be implemented in a subclass")
+        expandedNodes = []
+
+        upNode = self._moveUp(node)
+        if upNode:
+            expandedNodes.append(upNode)
+
+        downNode = self._moveDown(node)
+        if downNode:
+            expandedNodes.append(downNode)
+
+        leftNode = self._moveLeft(node)
+        if leftNode:
+            expandedNodes.append(leftNode)
+
+        rightNode = self._moveRight(node)
+        if rightNode:
+            expandedNodes.append(rightNode)
+
+        return expandedNodes
 
     def hashableState(self, node: Node):
-        raise NotImplementedError("hashState must be implemented in a subclass")
+        return node.state
