2e48a6ca0c94a75ed8a055af281dc6fd16e0da0a
Nuno Rodrigues <nunotechwork@gmail.com>
2025-04-05 10:45:40 +0100

    Remove C++ code

--- Cpp/src/algorithms/helpers/Node.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by nunorl on 3/10/25.
-//
-
-#include "Node.h"
--- Cpp/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-#include "algorithms/bestFirstSearch.h"
-#include "algorithms/helpers/Problem.h"
-
-int main()
-{
-}
